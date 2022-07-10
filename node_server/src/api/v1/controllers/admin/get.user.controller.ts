c65a05786a11425e69fb9dbda1f757412b53a9fc
RahulDutta007 <rahul39dutta@gmail.com>
2022-07-10 14:59:21 +0530

    check email/username exist endpoint

--- node_server/src/api/v1/controllers/admin/get.user.controller.ts
+++ node_server/src/api/v1/controllers/admin/get.user.controller.ts
@@ -1,18 +1,32 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import MESSAGE from "../../../../constants/message";
+import AdminModel from "../../../../models/Admin/admin.register.model";
+import service from "../../../../services";
 
 export const FindUsername = async (req: Request, res: Response) => {
 	try {
-		if (req.user._id) {
+		const user_id = req.query.user_name;
+		if (!user_id) {
+			return res.status(StatusCodes.BAD_REQUEST).json({
+				message: MESSAGE.get.fail,
+				result: "Please provide username"
+			});
+		}
+		const filter = {
+			user_name: user_id
+		};
+		const UserInstance = await service.query.fetchOne(AdminModel, filter);
+		if (UserInstance) {
 			return res.status(StatusCodes.OK).json({
 				message: MESSAGE.get.succ,
-				result: `Your username: ${req.user.user_name}`
+				result: `Username: ${UserInstance.user_name} already exist`
 			});
 		}
 		else {
-			return res.status(StatusCodes.BAD_REQUEST).json({
-				message: MESSAGE.get.fail
+			return res.status(StatusCodes.OK).json({
+				message: MESSAGE.get.succ,
+				result: `Username: ${user_id} does not exist`
 			});
 		}
 
@@ -27,15 +41,27 @@ export const FindUsername = async (req: Request, res: Response) => {
 
 export const FindEmail = async (req: Request, res: Response) => {
 	try {
-		if (req.user._id) {
+		const email = req.query.email;
+		if (!email) {
+			return res.status(StatusCodes.BAD_REQUEST).json({
+				message: MESSAGE.get.fail,
+				result: "Please provide email"
+			});
+		}
+		const filter = {
+			email
+		};
+		const UserInstance = await service.query.fetchOne(AdminModel, filter);
+		if (UserInstance) {
 			return res.status(StatusCodes.OK).json({
 				message: MESSAGE.get.succ,
-				result: `Your registered email: ${req.user.email}`
+				result: `Email: ${UserInstance.email} already exist`
 			});
 		}
 		else {
-			return res.status(StatusCodes.BAD_REQUEST).json({
-				message: MESSAGE.get.fail
+			return res.status(StatusCodes.OK).json({
+				message: MESSAGE.get.succ,
+				result: `Email: ${email} does not exist`
 			});
 		}
 
