cccd2e4d306f4a6762709acdc216088e1572530a
RahulDutta007 <rahul39dutta@gmail.com>
2022-08-12 14:28:25 +0530

    feat: add forget user id route

--- node_server/src/api/v1/routes/admin/user.management.routes.ts
+++ node_server/src/api/v1/routes/admin/user.management.routes.ts
@@ -6,7 +6,7 @@ import { GetToken, VerifyToken } from "../../controllers/admin/forget.password.c
 import { FindUsername, FindEmail, FindUserDetails, SendOTP, VerifyOTP } from "../../controllers/admin/get.user.controller";
 import verifyToken from "../../../../middlewares/auth/verifyToken.middleware";
 import { validators } from "../../validators";
-import { ForgetUserName } from "../../controllers/admin/forget.username.controller";
+import { ForgetUserId } from "../../controllers/admin/forget.username.controller";
 
 const router = express.Router();
 
@@ -16,7 +16,7 @@ router
 	.patch(validator(validators.changePasswordValidator, null), verifyToken, ChangePassword);
 router.route("/forget-password/get-token").post(validator(validators.forgetPasswordValidator, null), GetToken);
 router.route("/forget-password/verify-token/:token").post(validator(validators.verifyPasswordValidator, null), VerifyToken);
-router.route("/forget-user-name").post(ForgetUserName);
+router.route("/forget-user-name").post(validator(validators.forgetUserIdValidator, null), ForgetUserId);
 router.route("/find-user-name").get(verifyToken, FindUsername);
 router.route("/find-email").get(FindEmail);
 router.route("/send-otp").patch(SendOTP);
