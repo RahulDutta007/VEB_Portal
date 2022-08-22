bbe0b0da33da1c80e0281104755ddb776c8fda86
RahulDutta007 <rahul39dutta@gmail.com>
2022-08-22 20:09:34 +0530

    feat: add ag grid

---
+++ web_admin/src/components/pages/userManagement/UserManagement.tsx
@@ -0,0 +1,29 @@
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+import { CellValueChangedEvent, ColumnApi, GridApi, GridReadyEvent } from "ag-grid-community";
+
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import { useRef, useState } from "react";
+
+const UserManagement = (): JSX.Element => {
+	const [agGridAPI, setAgGridAPI] = useState<GridApi | null>(null);
+	const [agGridColumnAPI, setAgGridColumnAPI] = useState<ColumnApi | null>(null);
+	const agGridRef = useRef<any>(null);
+
+	const handleAgGridReady = (params: GridReadyEvent) => {
+		const { api, columnApi } = params;
+		setAgGridAPI(api);
+		setAgGridColumnAPI(columnApi);
+		api.sizeColumnsToFit();
+	};
+
+	const handleFirstDataRendered = (params: any) => null;
+
+	return (
+		<div className="user-management">
+			
+		</div>
+	);
+};
+
+export default UserManagement;
