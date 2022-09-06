d02402f3092304af9e2f45eb877b949f32ae7ed0
RahulDutta007 <rahul39dutta@gmail.com>
2022-09-06 18:32:40 +0530

    feat: change password + edit group owner validator routing

--- node_server/src/api/v1/validators/index.ts
+++ node_server/src/api/v1/validators/index.ts
@@ -2,11 +2,12 @@ import testValidator from "./test/test.validator";
 import adminRegisterValidator, { enrollerRegisterValidator } from "./admin/admin.register.validators";
 import { loginValidator } from "./admin/login.validator";
 import { adminCreationValidator } from "./admin/admin.creation.validator";
-import { changePasswordValidator } from "./admin/change.password.validator";
+import { changePasswordValidator } from "./auth/changePassword.validator";
 import { forgetPasswordValidator, verifyPasswordValidator } from "./admin/forgot.password.validator";
 import { sendOTPToEmailValidator, verifyOTPValidator } from "./OTP/OTP.validator";
 import { forgetUserIdValidator } from "./admin/forgetUserId.validator";
 import { planCreationValidator } from "./plan/plan.creation.validator";
+import { editGroupOwnerValidator } from "./auth/editGroupOwner.validator";
 
 export const validators = {
 	testValidator,
@@ -14,10 +15,11 @@ export const validators = {
 	enrollerRegisterValidator,
 	loginValidator,
 	adminCreationValidator,
-	changePasswordValidator,
 	forgetUserIdValidator,
 	forgetPasswordValidator,
 	verifyPasswordValidator,
+	editGroupOwnerValidator,
+	changePasswordValidator,
 	sendOTPToEmailValidator,
 	verifyOTPValidator,
 	planCreationValidator
